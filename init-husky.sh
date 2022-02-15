47a1e18aa03297da1297c1c5b628a10a7bb1f182
唐道源 <tangdaoyuan@megvii.com>
2022-02-15 14:53:32 +0800

    fix: husky install error

--- init-husky.sh
+++ init-husky.sh
@@ -1,3 +1,6 @@
-npm run prepare
-npx husky add .husky/pre-commit "npx lint-staged"
-npx husky add .husky/commit-msg "npx --no-install commitlint --edit "$1""
+#!/bin/sh
+if [ ! -d ".husky" ]; then
+  npx husky install
+  npx husky add .husky/pre-commit "npx lint-staged"
+  npx husky add .husky/commit-msg "npx --no-install commitlint --edit "$1""
+fi
